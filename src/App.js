7fd2419faac03b1f2a414babcc39cbf064e7aef0
hsynmrzyv <huseynmirzayev@outlook.com>
2022-07-16 16:09:42 +0400

    Styling finished

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+// Components
+import Review from "./Review";
+
+const App = () => {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+};
+
+export default App;
